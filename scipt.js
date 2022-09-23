d043bfce3e999db043d7689e21e8c31998c34adc
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-24 02:48:43 +0530

    Create grid using CssGrid instead of flex

--- scipt.js
+++ scipt.js
@@ -1,32 +1,42 @@
-const container = document.querySelector('.container');
+const container = document.getElementById('container');
 
 const newRowDiv = [];
 const newColumnDiv = [];
 
-// using flexbox
+//using cssGrid
 function createGrid(row,column){
-    for(let i=0; i<column; i++){
-        newRowDiv[i] = document.createElement('div');
-        newRowDiv[i].classList.add("new-div");
-        container.appendChild(newRowDiv[i]);
-        for(let j=0; j<row; j++){
-            newColumnDiv[j] = document.createElement('div');
-            newColumnDiv[j].classList.add("more-new-div");
-            newRowDiv[i].appendChild(newColumnDiv[j]);
-        }
+    container.style.setProperty('--gridRow', row);
+    container.style.setProperty('--gridColumn', column);
+    for(let i=0; i<(row*column); i++){
+        let newRowDiv = document.createElement('div');
+        newRowDiv.classList.add("new-div");
+        container.appendChild(newRowDiv);
     }
     
 }
 
 function hoverEffect(){
-    [...document.querySelectorAll('.more-new-div')].forEach(function(item){
+    [...document.querySelectorAll('.new-div')].forEach(function(item){
         item.addEventListener('mouseover',function(){
             item.classList.add('black-div');
         });
     });
 }
 
-createGrid(100,100);
+createGrid(10,10);
 hoverEffect();
 
-
+// using flexbox
+// function createGrid(row,column){
+//     for(let i=0; i<column; i++){
+//         newRowDiv[i] = document.createElement('div');
+//         newRowDiv[i].classList.add("new-div");
+//         container.appendChild(newRowDiv[i]);
+//         for(let j=0; j<row; j++){
+//             newColumnDiv[j] = document.createElement('div');
+//             newColumnDiv[j].classList.add("more-new-div");
+//             newRowDiv[i].appendChild(newColumnDiv[j]);
+//         }
+//     }
+    
+// }
