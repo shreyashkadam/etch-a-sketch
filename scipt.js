5ddf4a5ffc86cc308b5caaf00107466ab6e54d30
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-24 01:46:23 +0530

    Add hover effect on divs

--- scipt.js
+++ scipt.js
@@ -3,8 +3,7 @@ const container = document.querySelector('.container');
 const newRowDiv = [];
 const newColumnDiv = [];
 
-
-
+// using flexbox
 function createGrid(row,column){
     for(let i=0; i<column; i++){
         newRowDiv[i] = document.createElement('div');
@@ -19,4 +18,15 @@ function createGrid(row,column){
     
 }
 
-createGrid(16,16);
+function hoverEffect(){
+    [...document.querySelectorAll('.more-new-div')].forEach(function(item){
+        item.addEventListener('mouseover',function(){
+            item.classList.add('black-div');
+        });
+    });
+}
+
+createGrid(100,100);
+hoverEffect();
+
+
