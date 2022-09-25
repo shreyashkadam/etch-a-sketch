45a20052fa47f0a91ee79c9bb4934492d4b1fc8b
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-25 11:47:18 +0530

    Reformat code

--- scipt.js
+++ scipt.js
@@ -52,6 +52,15 @@ refresh.addEventListener('click', () => {
     window.location.reload();
 })
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 createGrid(30,30);
 hoverEffect();
 
